bd195a06e731be41c8579ea39bdff6082638db90
home <adhyanpatel@gmail.com>
2022-10-13 12:30:18 -0600

    [DEV] 1.3.0 - implementing a tokenizer instead of just simple regex

--- linter.py
+++ linter.py
@@ -1,7 +1,10 @@
-import re
+from itertools import chain
 import os
 
 
+
+def get_all_files():
+    return os.listdir(os.getcwd())
 
 def main():
     files = get_all_files()
@@ -9,93 +12,76 @@ def main():
     for i in files:
         if i not in except_src:
             try:
-                code_formatter(i)
+                lint(i)
                 print(f"[LINTED] {i}")
             except IsADirectoryError:
                 print(f"[SKIPPED] {i} is a directory")
+        else:
+            print(f"[SKIPPED] {i} found in exception list")
 
-def get_all_files():
-     return os.listdir(os.getcwd())
+'''
+    content = re.sub("[{]","\n    {\n",content)
+    content = re.sub("[}]","\n    }\n",content)
+'''
 
-def code_formatter(fileName, passes=3):
+def lint(fileName) -> None:
     with open(fileName, "r") as fobj:
         content = fobj.read()
-        content = re.sub("[{]","\n    {",content)
-        content = re.sub("[}]","    }\n",content)
 
-        content = [x for x in content.splitlines()] # if "\n" not in x and x.strip() != ""]
-        content = "\n".join(content)
+    tokens = tokeniser(content)
+    bracket_aligned = manage_brackets(tokens)
+    indent_aligned = manage_indents(bracket_aligned)
 
-    count = 0
-    while count < passes:
-        content = fix_indent_levels(content)
-        count += 1
-#    content = remove_bracket_newlines(content)
-
-    with open(fileName, "w") as fobj:
-        fobj.write(content)
-
-def fix_indent_levels(content): 
-    '''
-    { => aligns to next line of code
-
-    } => aligns to previous line of code 
-    '''
-    opening_brace = "{"
-    closing_brace = "}"
-    linesplit_li = content.splitlines()
-    processed_li = []
-
-    for x, i in enumerate(linesplit_li):
-        if opening_brace in i:
-            next_indent_level = get_indent_level(linesplit_li[x + 1])
-            curr_indent_level = get_indent_level(i)
-            fixed = match_indent_levels(next_indent_level, curr_indent_level, i)
-            processed_li.append(fixed)
-
-        elif closing_brace in i:
-            prev_indent_level = get_indent_level(linesplit_li[x - 1])
-            curr_indent_level = get_indent_level(i)
-            fixed = match_indent_levels(prev_indent_level, curr_indent_level, i)
-            processed_li.append(fixed)
-
-        else:
-            processed_li.append(i)
-
-    matched_indents = "\n".join(processed_li)
-
-    return matched_indents
-
-def get_indent_level(line):
-    count = 0
-    for i in line:
-        if not i.isalnum():
-            if i == " ":
-                count += 1
-            elif i == "\t":
-                count += 4
-        else:
-            break
-
-    return count
+    return None
 
 
-def match_indent_levels(tomatch, current, str):
-    if tomatch > current:
-        return " " * (tomatch - current) + str
-    elif tomatch < current:
-        str = str.lstrip()
-        return " " * tomatch + str
-    else: 
-        return str
+def tokeniser(content) -> list:
+    lines = content.split("\n")
+    return lines
 
 
+def manage_brackets(tokens):
+    open_brace = "{"
+    close_brace = "}"
+    
+    bracket_aligned = []
 
-def check_function_complexity(fileName):
-     pass
+    for idx, i in enumerate(tokens):
+        for sub_idx, j in enumerate(i):
+            curr = {"line num": idx, "char num": sub_idx, "char": j} # numbers are offset by 1
+            if curr["char"] == open_brace:
+                look_back = -1
+                while True:
+                    if (bracket_aligned[look_back].isalnum() or bracket_aligned[look_back].isspace()) and not ("\n" in bracket_aligned[look_back]) :
+                        bracket_aligned.append("\n")
+                    elif "\n" in bracket_aligned[look_back]:
+                        break
+                    look_back -= 1
+                bracket_aligned.append(curr["char"])
+            elif curr["char"] == close_brace:
+                look_ahead = -1
+                while True:
+                    if (bracket_aligned[look_ahead].isalnum()) and not ("\n" in bracket_aligned[look_ahead]) :
+                        bracket_aligned.append("\n")
+                    elif "\n" in bracket_aligned[look_back]:
+                        break
+                    look_back -= 1
+                bracket_aligned.append(curr["char"])
+            else:
+                bracket_aligned.append(curr["char"])
 
-def check_McGabe_complexity(fileName):
-     pass
+
+            print(curr)
+
+        bracket_aligned.append("\n")
+        print(idx)
+
+
+    return bracket_aligned
+
+
+def manage_indents(content):
+    return content
 
 
 
