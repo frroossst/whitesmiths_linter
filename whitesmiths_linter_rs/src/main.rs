d5b8afd1305088eb02a8b87f77c3272e1f5f1f9b
Adhyan H <adhyanpatel@gmail.com>
2023-06-21 19:53:42 -0600

    asf

--- whitesmiths_linter_rs/src/main.rs
+++ whitesmiths_linter_rs/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
-}
+fn read_file() 
+    {
+
+    }
+
+fn add_newlines() 
+    {
+
+    }
+
+fn adjust_indentation() 
+    {
+
+    }
+
+fn main() 
+    {
+    read_file();
+    add_newlines();
+    adjust_indentation();
+    }
